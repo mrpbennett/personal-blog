032ae0b3511302d56a6b748e69ef87d4b72717c0
Paul Bennett <pbennett.uk@gmail.com>
2024-06-12 11:27:39 +0100

    adding new colours to prismjs

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,13 @@ import Prism from 'prismjs'
 import PropTypes from 'prop-types'
 import React, {useEffect} from 'react'
 
+require('prismjs/components/prism-javascript')
+require('prismjs/components/prism-css')
+require('prismjs/components/prism-jsx')
+require('prismjs/components/prism-sql')
+require('prismjs/components/prism-yaml')
+require('prismjs/components/prism-bash')
+
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
